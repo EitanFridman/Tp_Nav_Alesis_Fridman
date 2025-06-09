4d668a021a7997fa13ff634439a2dea888527b3d
EITÁN GABRIEL FRIDMAN <48242472@ort>
2025-06-09 10:40:28 -0300

    tp listo

--- screens/HomeDetailScreen.js
+++ screens/HomeDetailScreen.js
@@ -7,9 +7,10 @@ export default function HomeDetailScreen({ navigation }) {
     <View style={[styles.container, { backgroundColor: '#cde1f9' }]}>
       <Text style={styles.text}>Detalle de Home Screen</Text>
       <Image
-        source={{ uri: 'https://m.media-amazon.com/images/I/61TGfufxpGL.jpg' }}
+        source={{ uri: 'https://blixtdev.com/content/images/2023/01/1-4bIsUtmfOQD45_2dt_Of5Q.png' }}
         style={{ width: '100%', height: '100%' }}
       />
     </View>
   );
 }
+
